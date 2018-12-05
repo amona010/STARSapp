cc27bb8d70324d98d17892b6acf15d7d70d5438d
dominique felix <dfeli014@naboo.cs.fiu.edu>
2018-12-04 19:46:00 -0500

    Core Data implementation and Camera Functionality
    Core Data implementation
    Camera functionality

--- STARSapp/STARSapp/appointmentsTableViewController.swift
+++ STARSapp/STARSapp/appointmentsTableViewController.swift
@@ -8,11 +8,18 @@
 
 import Foundation
 import UIKit
+import CoreData
 
+class tutorAppointmentCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var apptLabel: UILabel!
+    @IBOutlet var phoneLabel: UILabel!
+}
 
-class appointmentsTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class appointmentsTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
     
     @IBOutlet var appointmentTable: UITableView!
+    var managedObjectContext: NSManagedObjectContext? = nil
     
     var data = modelData.getSome
     
@@ -32,12 +39,59 @@ class appointmentsTableViewController: UIViewController, UITableViewDelegate, UI
         // Dispose of any resources that can be recreated.
     }
     
+    var fetchedResultsController: NSFetchedResultsController<Appointment> {
+        if _fetchedResultsController != nil {
+            return _fetchedResultsController!
+        }
+        
+        let fetchRequest: NSFetchRequest<Appointment> = Appointment.fetchRequest()
+        
+        // Set the batch size to a suitable number.
+        fetchRequest.fetchBatchSize = 20
+        
+        // Edit the sort key as appropriate.
+        let sortDescriptor = NSSortDescriptor(key: "tutorName", ascending: true)
+        
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        // Edit the section name key path and cache name if appropriate.
+        // nil for section name key path means "no sections".
+        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
+        aFetchedResultsController.delegate = self
+        _fetchedResultsController = aFetchedResultsController
+        
+        do {
+            try _fetchedResultsController!.performFetch()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+        
+        return _fetchedResultsController!
+    }
+    
+    var _fetchedResultsController: NSFetchedResultsController<Appointment>? = nil
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        let sectionInfo = fetchedResultsController.sections![section]
+        return sectionInfo.numberOfObjects
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "appointmentCell", for: indexPath) as! tutorAppointmentCell
+        let appt = fetchedResultsController.object(at: indexPath)
+        
+        cell.nameLabel.text = appt.tutorName
+        cell.apptLabel.text = appt.schedule
+        cell.phoneLabel.text = String(appt.phoneNumber)
+        
         return cell
+    }
+    
+    // Function for allowing tableView edits, which is set to true, since appointments can get cancelled.
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
     }
 }
