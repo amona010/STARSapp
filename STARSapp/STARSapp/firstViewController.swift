b1a252dc55b34a55d7ff4f0558f09031ab3e5723
Fazwa <Fawaz@Fs-MacBook-Pro.local>
2018-12-05 21:46:05 -0500

    validationAddition

--- STARSapp/STARSapp/firstViewController.swift
+++ STARSapp/STARSapp/firstViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class firstViewController: UIViewController {
+class firstViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var passWord: UITextField!
+    
+    let data = modelData.getSome
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,7 +25,36 @@ class firstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-
+    /*
+    Performs a login check before segueing into the application.
+    If incorrect returns false and an alert is shown
+     */
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        if data.isName(access: userName.text!) &&
+            data.isPass(access: passWord.text!)
+        {
+            return true
+        }
+        else{
+            let alert = UIAlertController(title: "Incorrect Login", message: "Incorrect Login. Please try again. ", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
+            userName.text = ""
+            passWord.text = ""
+            
+        }
+        
+        return false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool { 
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
 }
 
